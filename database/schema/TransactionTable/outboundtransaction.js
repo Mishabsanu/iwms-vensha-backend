4b832fc18b8b2ada089b4c33ed5ba14e759e771e
rahuljonwal247 <rahuljonwal247@gmail.com>
2024-09-02 10:11:31 +0530

    costomerchange

---
+++ database/schema/TransactionTable/outboundtransaction.js
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+const outboundtransactionModel = new mongoose.Schema({
+  order_qty: {
+    type: Number,
+    required: true,
+    trim: true,
+    default: 0,
+  },
+  order_number: {
+    type: Number,
+    required: true,
+    trim: true,
+    default: 0,
+  },
+  sku_code: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  order_type: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  sku_description: {
+    type: String,
+    trim: true,
+  },
+  sut: {
+    type: String,
+    trim: true,
+  },
+  uom: {
+    type: String,
+    trim: true,
+  },
+  bin: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  assigned_to: {
+    type: mongoose.Types.ObjectId,
+    ref: "users",
+    required: true,
+    trim: true,
+  },
+ 
+  customerDetails: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+ 
+  date: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  digit_3_codes: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  batch: {
+    type: String,
+    trim: true,
+    default: null,
+  },
+  status: { type: String, default: "Pending" },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  deleted_at: { type: Date, default: null },
+});
+
+const OutboundTransactionModel = mongoose.model("outboundtransactionModel", outboundtransactionModel);
+
+export default OutboundTransactionModel;
+
